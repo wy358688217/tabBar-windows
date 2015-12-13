4cc89c98017ab4812798b0c130e0cf7947761db3
Mr.Wang <wang@WangdeMacBook-Pro.local>
2015-12-13 20:20:49 +0800

    add a  viewModel file for windowsView
    增加一个view model 为后续功能实现提供保障

---
+++ tabbarTest/WindowsViewModel.h
@@ -0,0 +1,13 @@
+//
+//  WindowsViewModel.h
+//  tabbarTest
+//
+//  Created by wang on 15/12/13.
+//  Copyright © 2015年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WindowsViewModel : UIView
+
+@end
---
+++ tabbarTest/WindowsViewModel.m
@@ -0,0 +1,27 @@
+//
+//  WindowsViewModel.m
+//  tabbarTest
+//
+//  Created by wang on 15/12/13.
+//  Copyright © 2015年 Kevin. All rights reserved.
+//
+
+#import "WindowsViewModel.h"
+
+@implementation WindowsViewModel
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    
+}
+
+@end
